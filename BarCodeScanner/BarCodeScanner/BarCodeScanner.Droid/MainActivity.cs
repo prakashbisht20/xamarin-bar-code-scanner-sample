01af49fe4d8532286f4d0945c0503555d54c0aed
HOME\Hp <psb.bisht20@gmail.com>
2025-07-02 21:31:24 +0530

    changes pushed to repo.

---
+++ BarCodeScanner/BarCodeScanner/BarCodeScanner.Droid/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+namespace BarCodeScanner.Droid
+{
+    [Activity(Label = "BarCodeScanner", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            global::Acr.BarCodes.BarCodes.Init(() => (Activity)Forms.Context);
+            LoadApplication(new App());
+        }
+    }
+}
+
---
+++ BarCodeScanner/BarCodeScanner/BarCodeScanner/HomePage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace BarCodeScanner
+{
+    public class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            Button scanBtn = new Button
+            {
+                Text = "Scan Barcode",
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+            };
+
+            scanBtn.Clicked += async (sender, args) =>
+            {
+                var scanResult = await Acr.BarCodes.BarCodes.Instance.Read();
+                if (!scanResult.Success)
+                {
+                    await this.DisplayAlert("Alert ! ", "Sorry ! \n Failed to read the Barcode !", "OK");
+                }
+                else
+                {
+                    await this.DisplayAlert("Scan Successful !", String.Format("Barcode Format : {0} \n Barcode Value : {1}", scanResult.Format, scanResult.Code), "OK");
+                }
+            };
+
+            Content = new StackLayout
+            {
+                Children = {
+                    scanBtn
+				}
+            };
+        }
+    }
+}
